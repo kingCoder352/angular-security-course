2eb4a1733aa47640362663569f17aae858ad754c
Jalen <jalenkbracey@gmail.com>
2020-02-12 06:25:53 -0500

    Stored password to the database using hashing and salting techniques

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {AuthService} from "../services/auth.service";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'signup',
@@ -9,23 +9,29 @@ import {AuthService} from "../services/auth.service";
 })
 export class SignupComponent implements OnInit {
 
-    form:FormGroup;
+    form: FormGroup;
+    errors: string[] = [];
+
+    messagePerErrorCode = {
+      min: 'The minimum length is 8 characters',
+      uppercase: 'Must contain at least one upper case character',
+      lowercase: 'Must contain at least one lower case character',
+      digits: 'Must contain at least one numeric character'
+    };
 
     constructor(private fb: FormBuilder, private authService: AuthService) {
 
         this.form = this.fb.group({
-            email: ['',Validators.required],
-            password: ['',Validators.required],
-            confirm: ['',Validators.required]
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+            confirm: ['', Validators.required]
         });
-
 
     }
 
     ngOnInit() {
 
     }
-
 
     signUp() {
         const val = this.form.value;
@@ -34,8 +40,8 @@ export class SignupComponent implements OnInit {
 
             this.authService.signUp(val.email, val.password)
                 .subscribe(
-                    () => console.log("User created successfully"),
-                    console.error
+                    () => console.log('User created successfully'),
+                    response => this.errors = response.error.errors
                 );
 
         }
